20ce6d1f9802082cf88f4c272d90406357800ebe
tokinantenaina-nt <tokib2oba@yahoo.com>
2022-05-02 22:13:15 +0300

    js morpion : finished of confirm message before rejouer if gameOver is false, not finished  table score

--- morpion.js
+++ morpion.js
@@ -7,22 +7,24 @@ let tablGame = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 const player = "X";
 const computer = "O";
 let actifPlayer = player;
-// ------------- Compteur
+let gameOver = false;
 
-let tablScorePlayer = [0, " "];
-let tablScoreComputer = [0, " "];
-// --------- Score -----------
+// ------------- Compteur de score -------------------
+
+let tablScorePlayer = [0, 0];
+let tablScoreComputer = [0, 0];
 let s = 0;
+
 function sumScore(tabl) {
-  for (let i = 0; i < tabl.length; i++) {
-    s = s + tabl[i];
+  if (gameOver == false) {
+    for (let i = 0; i < 2; i++) {
+      s = s + tabl[i];
+    }
+    // return s;
   }
-  return s;
 }
-
-// -------------------------
-
-// -----------------------------
+console.log(s);
+// -------------------------------------------------
 
 // ------------ WinTest : condition de victoire
 
@@ -37,41 +39,60 @@ function sumScore(tabl) {
 // ["3 ", " 4", "5 "];
 // ["6 ", " 7", "8 "];
 /*let z = 0, u = 1, d = 2; */
+// ------------ Lancement du jeu
 
+// ---------------------------------
+//-------------- Game Stop ------
+function gameStop() {
+  if (gameOver == true) {
+    // alert("You win!");
+  }
+}
+
+//-------------Fonction de Test victoire----------------
 function winTest(z, u, d) {
   if (
     tablGame[z] == tablGame[u] &&
     tablGame[u] == tablGame[d] &&
-    tablGame[d] == player
+    tablGame[d] == player &&
+    gameOver == false &&
+    actifPlayer === computer
   ) {
     console.log("You win !");
     document.getElementById("tourDeJeu").innerHTML =
       '<div style="color: darkgreen"> You win! </div>';
     tablScorePlayer[1] = 1;
-    console.log(sumScore(tablScorePlayer));
-    document.getElementById("youScore").innerText =
-      sumScore(tablScorePlayer) - 1;
+    sumScore(tablScorePlayer);
+    console.log("score", s);
+    document.getElementById("youScore").innerText = s;
+    return (gameOver = true);
   }
 
   if (
     tablGame[z] == tablGame[u] &&
     tablGame[u] == tablGame[d] &&
-    tablGame[d] == computer
+    tablGame[d] == computer &&
+    gameOver == false &&
+    actifPlayer === player
   ) {
     console.log("Computer win !");
     document.getElementById("tourDeJeu").innerHTML =
       '<div style="color: darkred"> Computer win! </div>';
     console.log(tablGame);
+    tablScoreComputer[1] = 1;
+    sumScore(tablScoreComputer);
+    console.log("score", s);
+    document.getElementById("cpuScore").innerText = s;
+    return (gameOver = true);
   }
 }
-
 //---------------------------------------------------------------
 
 // Récupération des cases à clicker
 
 function choiseCase(id) {
   let numCell = +id.match(/\d+/g).join("") - 1;
-  if (tablGame[numCell] != " ") {
+  if (tablGame[numCell] != " " && gameOver == false) {
     document.getElementById("dejaJouer").innerHTML =
       "Attention , Cette case est déjà jouée!";
     setTimeout(() => {
@@ -79,42 +100,63 @@ function choiseCase(id) {
     }, 2000);
     return;
   }
-  console.log(id);
-  console.log(numCell, "est la cellule cliquée");
-  tablGame[numCell] = actifPlayer;
-  console.clear();
+  if (gameOver == true) {
+    document.getElementById("dejaJouer").innerHTML =
+      "Veuillez cliquer sur rejouer pour une autre partie ! ";
+    setTimeout(() => {
+      document.getElementById("dejaJouer").innerHTML = "";
+    }, 2000);
+    return;
+  }
+  if (gameOver == false) {
+    console.log(id);
+    console.log(numCell, "est la cellule cliquée");
+    tablGame[numCell] = actifPlayer;
+    //console.clear();
 
-  console.log("avant test");
-  document.getElementById(id).innerHTML = actifPlayer;
-  actifPlayer = actifPlayer === player ? computer : player;
+    console.log("avant test");
+    document.getElementById(id).innerHTML = actifPlayer;
+    actifPlayer = actifPlayer === player ? computer : player;
 
-  document.getElementById("tourDeJeu").innerHTML =
-    // "C'est le tour de " + actifPlayer + " qui jouer";
-    "C'est à votre tour de jouer";
+    document.getElementById("tourDeJeu").innerHTML =
+      "C'est à votre tour de jouer";
 
-  winTest(0, 1, 2);
-  winTest(0, 3, 6);
-  winTest(0, 4, 8);
-  winTest(1, 4, 7);
-  winTest(2, 5, 8);
-  winTest(2, 4, 6);
-  winTest(3, 4, 5);
-  winTest(6, 7, 8);
+    winTest(0, 1, 2);
+    winTest(0, 3, 6);
+    winTest(0, 4, 8);
+    winTest(1, 4, 7);
+    winTest(2, 5, 8);
+    winTest(2, 4, 6);
+    winTest(3, 4, 5);
+    winTest(6, 7, 8);
+    gameStop();
+  }
+  if (gameOver == true) {
+    console.log(tablGame);
+  }
 }
 
 //Vide le contenu de toute les cases
 
 function reset() {
-  for (var i = 0; i < items.length; i++) {
-    console.log(items[i]);
-    items[i].textContent = " ";
-    console.clear();
-    tablGame.splice(0);
+  let conf = function () {
+    return confirm("Voulez-vous vraiment rejouer ?");
+  };
+
+  if (gameOver === true || (gameOver === false && conf() === true)) {
+    for (var i = 0; i < items.length; i++) {
+      console.log(items[i]);
+      items[i].textContent = " ";
+      //console.clear();
+    }
+    tablGame = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+    console.log(tablGame);
+    document.getElementById("dejaJouer").innerHTML = "";
+    document.getElementById("tourDeJeu").innerHTML = "";
+    console.log(gameOver);
+    gameOver = false;
+    console.log("score", s);
   }
-  tablGame = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
-  console.log(tablGame);
-  document.getElementById("dejaJouer").innerHTML = "";
-  document.getElementById("tourDeJeu").innerHTML = "";
 }
 
 console.log("ok");
